b77a531ecf54913e43a61cbb4ba0dca53d6b6157
Juarezd69 <brandon.juarezfernan@evsck12.com>
2021-11-12 06:30:23 -0600

    Started intro

--- PassWordMananger.py
+++ PassWordMananger.py
@@ -1,1 +1,6 @@
-yes
+print("Hello World")
+ui=input(f'''What may I do for you today?
+Would you like to (v)iew your passwords?
+Perhaps (m)ake a new passsword?
+(D)elete a password?
+Perhaps (c)hange one of your passwords?''')
